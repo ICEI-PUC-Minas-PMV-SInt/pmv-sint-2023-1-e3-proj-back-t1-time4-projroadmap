681109274ee0df6ac468996ac1bb8790268eedbf
Ramon Vicente <vicente.silva.ramon@gmail.com>
2023-03-22 22:28:52 -0300

    Criação do projeto com o framework e configuração do DbContext

---
+++ src/interview/interview/Models/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace interview.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Teste> Teste { get; set; }
+    }
+}
