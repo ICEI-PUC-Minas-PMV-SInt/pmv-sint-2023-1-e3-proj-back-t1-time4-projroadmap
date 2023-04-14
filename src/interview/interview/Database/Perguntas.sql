f0b3a4bffed57b49682976fe4f0bf591e2d56697
Paulo Henrique <phvcandido@gmail.com>
2023-04-13 23:52:00 -0300

    Adiciona modelo e script de banco para Perguntas

---
+++ src/interview/interview/Database/Perguntas.sql
@@ -0,0 +1,7 @@
+﻿-- Script de criação da tabela perguntas
+-- Necessário verificar se a abordagem será utilizando SQL ou Migrations
+CREATE TABLE Perguntas (
+	Id integer PRIMARY KEY AUTO_INCREMENT,
+	Description varchar(255),
+	idTema int -- adicionar posteriormente relacionamento com a tabela tema
+);
---
+++ src/interview/interview/Models/Perguntas.cs
@@ -0,0 +1,14 @@
+﻿namespace interview.Models
+{
+    public class Perguntas
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        // TODO: transformar em array de temas quando houver a classe
+        public int idTema { get; set; } 
+
+
+    }
+}
